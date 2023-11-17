e938b24e67ea801105d10cb23f8da64f10ce34f0
Felipe Zampa <86922370+felipezampa@users.noreply.github.com>
2023-11-16 21:29:54 -0300

    [add] models no painel admin

--- back/core/admin.py
+++ back/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .db_models.adress_related_models import *
 # Register your models here.
+
+admin.site.register(Country)
+admin.site.register(State)
+admin.site.register(City)
