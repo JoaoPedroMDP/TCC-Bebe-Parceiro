93e6bdb10132896599956b26ad523d1010d48b9b
joao <joao.pedro@pop-pr.rnp.br>
2024-03-26 13:49:23 -0300

    wip: Tentando adicionar login nos testes

--- back/tests/access_code/test_can_list_all_access_codes.py
+++ back/tests/access_code/test_can_list_all_access_codes.py
@@ -1,15 +1,32 @@
 #  coding: utf-8
+import logging
+
 import pytest
+from django.contrib.auth.models import Group
+from django.db.models.base import ModelBase
 from django.test.client import Client
 from django.urls import reverse
 
+from core.models import User
 from factories import AccessCodeFactory
 
 
-@pytest.mark.django_db
-def test_can_list_all_access_codes(client: Client):
-    codes = AccessCodeFactory.create_batch(size=5)
+lgr = logging.getLogger(__name__)
 
+
+@pytest.mark.django_db
+def test_can_list_all_access_codes(client: Client, django_user_model: ModelBase):
+    codes = AccessCodeFactory.create_batch(size=5)
+    
+    group = Group.objects.filter(name="manage_access_codes").first()
+    lgr.debug(type(django_user_model))
+    ac_user = django_user_model.objects.create_user(username="ac_user", password="ac_user")
+    ac_user.groups.add(group)
+    
+    client.force_login(ac_user)
+    
     url = reverse("gen_access_codes")
     response = client.get(url)
+    lgr.debug(response.data)
+    assert response.status_code == 200
     assert len(response.data) == len(codes)
--- back/tests/conftest.py
+++ back/tests/conftest.py
@@ -2,9 +2,11 @@
 from datetime import time
 
 import pytest
+from django.contrib.auth.models import Group
 
 from config import GROUPS, ROLES
-from factories import GroupFactory
+from core.models import User
+from factories import GroupFactory, UserFactory
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -24,3 +26,11 @@ def seed_db(db):
 
     for r in ROLES:
         GroupFactory.create(name=r)
+
+
+# @pytest.fixture()
+# def ac_user(db):
+#     group: Group = Group.objects.get(name='manage_access_codes')
+#     user: User = UserFactory.create(username="ac_user", password="ac_user")
+#     user.groups.add(group)
+#     yield user
