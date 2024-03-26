ae9a6010c7037429f113bf97d4f12cf775dfc25c
joao <joao.pedro@pop-pr.rnp.br>
2024-03-26 09:50:06 -0300

    add(tests, access_code): Teste de criação de múltiplos códigos de acesso

--- back/tests/access_code/test_can_create_access_code.py
+++ back/tests/access_code/test_can_create_access_code.py
@@ -13,3 +13,17 @@ def test_can_create_access_code(client: Client):
     assert response.status_code == 201
     assert response.data['code'].startswith("TCCAC") is True
     assert response.data['used'] is False
+
+
+@pytest.mark.django_db
+def test_can_create_multiple_access_codes(client: Client):
+    amount = 5
+    data = {'prefix': "TCCMAC", "amount": amount}
+    url = reverse('gen_access_codes')
+    response = client.post(url, data=data)
+
+    assert response.status_code == 201
+    assert len(response.data) == amount
+    for code in response.data:
+        assert code['code'].startswith("TCCMAC") is True
+        assert code['used'] is False
