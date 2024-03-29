7784141ae0194c3e218c3fd5119a71af697a25d0
JoaoPedroMDP <joao.pedro.mdp@outlook.com>
2024-03-28 21:59:45 -0300

    change: Não tem mais prefix em Access Codes

--- back/tests/access_code/test_access_code_creation.py
+++ back/tests/access_code/test_access_code_creation.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 
 from config import MANAGE_ACCESS_CODES
+from core.models import User
 from tests.conftest import make_user
 
 lgr = logging.getLogger(__name__)
@@ -13,19 +14,19 @@ lgr = logging.getLogger(__name__)
 
 @pytest.mark.django_db
 def test_can_create_access_code(client: APIClient):
-    data = {'prefix': "TCCAC"}
     url = reverse('gen_access_codes')
 
     # Sem autenticação
-    response = client.post(url, data=data)
+    response = client.post(url)
     assert response.status_code == 401
 
     # Com autenticação
-    client.force_authenticate(make_user([MANAGE_ACCESS_CODES]))
-    response = client.post(url, data=data)
-    lgr.debug(response.data)
+    user: User = make_user([MANAGE_ACCESS_CODES])
+    client.force_authenticate(user)
+    response = client.post(url)
+    prefix = user.first_name[:3].upper()
     assert response.status_code == 201
-    assert response.data[0]['code'].startswith("TCCAC") is True
+    assert response.data[0]['code'].startswith(prefix) is True
     assert response.data[0]['used'] is False
 
 
