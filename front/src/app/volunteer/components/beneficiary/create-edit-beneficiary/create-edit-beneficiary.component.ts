9b0bf1d56905cbf5bac6c50f4a1f98136936d9db
Felipe Zampa <86922370+felipezampa@users.noreply.github.com>
2024-04-07 21:58:40 -0300

    [add] crud beneficiada - create e edit inicial

---
+++ front/src/app/volunteer/components/beneficiary/create-edit-beneficiary/create-edit-beneficiary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-edit-beneficiary',
+  templateUrl: './create-edit-beneficiary.component.html',
+  styleUrls: ['./create-edit-beneficiary.component.css']
+})
+export class CreateEditBeneficiaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
