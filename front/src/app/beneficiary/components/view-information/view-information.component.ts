58de28a3d5b80e6af5bca298eabbe3b0fe502234
Felipe Zampa <86922370+felipezampa@users.noreply.github.com>
2024-04-22 15:26:29 -0300

    Update view-information.component.ts

--- front/src/app/beneficiary/components/view-information/view-information.component.ts
+++ front/src/app/beneficiary/components/view-information/view-information.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth';
 import { Beneficiary, SwalFacade, UserToken } from 'src/app/shared';
 import { BeneficiaryService } from '../../services/beneficiary.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AuthService } from 'src/app/auth';
+import { RemoveInformationComponent } from '../remove-information/remove-information.component';
 
 @Component({
   selector: 'app-view-information',
@@ -12,12 +15,13 @@ import { AuthService } from 'src/app/auth';
 export class ViewInformationComponent implements OnInit {
 
   beneficiary!: Beneficiary;
+  subscription: Subscription | undefined;
 
   constructor(
     private beneficiaryService: BeneficiaryService,
     private authService: AuthService,
-    private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private modalService: NgbModal
   ) { }
 
   ngOnInit(): void {
@@ -34,12 +38,16 @@ export class ViewInformationComponent implements OnInit {
     });
   }
 
-
   editBeneficiary() {
-
+    // Fazer
   }
 
+  /**
+   * @description Abre um modal para excluir a beneficiada
+   */
   deleteBeneficiary() {
-
+    this.modalService.open(
+      RemoveInformationComponent, { size: 'xl' }
+    ).componentInstance.beneficiary = this.beneficiary!;
   }
 }
