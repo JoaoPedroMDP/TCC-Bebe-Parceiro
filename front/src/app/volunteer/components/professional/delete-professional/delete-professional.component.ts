d3cab907b73efa9eada1f21cde101c8555ab937a
Felipe Zampa <86922370+felipezampa@users.noreply.github.com>
2024-04-26 15:51:39 -0300

    [change] delete profissional ok e um tratamento na listagem dos pendentes

--- front/src/app/volunteer/components/professional/delete-professional/delete-professional.component.ts
+++ front/src/app/volunteer/components/professional/delete-professional/delete-professional.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { SwalFacade } from 'src/app/shared';
+import { Professional } from 'src/app/shared/models/professional';
+import { ProfessionalService } from 'src/app/volunteer/services/professional.service';
 
 @Component({
   selector: 'app-delete-professional',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteProfessionalComponent implements OnInit {
 
-  constructor() { }
+  @Input() professional!: Professional;
+
+  constructor(public activeModal: NgbActiveModal, public professionalService: ProfessionalService) { }
 
   ngOnInit(): void {
   }
 
+  /**
+   * @description Executa o método deleteProfessional() do professionalService e retorna uma mensagem 
+   * de sucesso ou erro a depender do resultado da operação
+   */
+  deleteProfessional() {
+    this.professionalService.deleteProfessional(this.professional.id!).subscribe({
+      next: () => {
+        this.activeModal.close();
+        SwalFacade.success("Profissional excluído", `${this.professional?.name} foi excluído com sucesso!`)
+      },
+      error: (e) => SwalFacade.error("Ocorreu um erro!", `Não foi possível fazer excluir o profssional: ${e}`)
+    })
+  }
 }
