73c8f01a11efa0895e5852301571d0af3b88dd52
Felipe Zampa <86922370+felipezampa@users.noreply.github.com>
2024-05-09 23:56:05 -0300

    [change] add urls para os termo/campanha e outras pequenas alteracoes

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   recaptchaSiteKey: '6Ld-QrwpAAAAAGnCQb9APLG7vtR3Z3Bl0f0EsQbC',
-  baseURL: 'http://localhost:8000/'
+  baseURL: 'http://localhost:8000/',
+  professionalTermURL: 'https://docs.google.com/document/u/0/'
 };
