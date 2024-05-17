75fe1fa41b6364be796019eca8e595e5119af6fe
JoaoPedroMDP <joao.pedro.mdp@outlook.com>
2024-05-17 17:19:53 -0300

    fix: Removendo senha do config.py

--- back/config.py
+++ back/config.py
@@ -4,7 +4,7 @@ import os
 
 # DATABASE DATABASE DATABASE DATABASE DATABASE
 DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "osemp231")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
 DB_NAME = os.getenv("DB_NAME", "bebeparceiro")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
