2105104c5d2382a64f16648d5e804e77997d3a82
JoaoPedroMDP <joao.pedro.mdp@outlook.com>
2024-06-11 21:09:54 -0300

    wip: Melhorando seeder para ter dados mais consistentes e que podem ser apresentados no TCC

--- back/core/management/commands/seed.py
+++ back/core/management/commands/seed.py
@@ -6,7 +6,8 @@ from django.core.management.base import BaseCommand
 from django.utils.timezone import now
 
 from config import CLOTH_SIZES, CLOTH_TYPE, GROUPS, SHOE_SIZES, SHOE_TYPE, STATUSES, MARITAL_STATUSES, SOCIAL_PROGRAMS
-from core.models import User
+from core.management.commands import ADMIN_DATA, BENEFICIARIES, SWAP_BENEFICIARIES
+from core.models import Beneficiary, Child, City, MaritalStatus, SocialProgram, User
 from factories import AppointmentFactory, MaritalStatusFactory, RegisterFactory, SocialProgramFactory, SizeFactory, CountryFactory, StateFactory, CityFactory, \
     AccessCodeFactory, SwapFactory, UserFactory, BeneficiaryFactory, ChildFactory, VolunteerFactory, GroupFactory, StatusFactory, CampaignFactory
 
@@ -17,11 +18,47 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("--test", action='store_true', help="Se deve criar dados para testes manuais")
 
+    @staticmethod
+    def create_beneficiary(ben: dict, cities: list[City], mar_stats: list[MaritalStatus], soc_progs: list[SocialProgram]):
+        u = UserFactory.create(**ben['user'])
+        ben['user'] = u
+        ben['city'] = cities[ben['city']]
+        marital_status = mar_stats[ben.pop('marital_status')]
+        social_programs = [soc_progs[s] for s in ben.pop('social_programs')]
+
+        children_data = ben.pop('children_data')
+        
+        b = BeneficiaryFactory.create(**ben)
+        b.marital_status = marital_status
+        b.social_programs.set(social_programs)
+
+        children = []
+        for child_data in children_data:
+            children.append(ChildFactory.create(beneficiary=b, **child_data))
+        
+        return b, children
+
+    @staticmethod
+    def create_swap(swap: dict, beneficiary: Beneficiary, child: Child, shoe_sizes: dict, cloth_sizes: dict, statuses: dict):
+        swap['beneficiary'] = beneficiary
+        swap['child'] = child
+        swap['cloth_size'] = cloth_sizes[swap.pop('cloth_size')]
+        if 'shoe_size' in swap:
+            swap['shoe_size'] = shoe_sizes[swap.pop('shoe_size')]
+        swap['status'] = statuses[swap.pop('status')]
+        SwapFactory.create(**swap)
+
+
     def handle(self, *app_labels, **options):
         # CEP
         brazil = CountryFactory.create(name="Brasil", enabled=True)
         parana = StateFactory.create(name="Paraná", country=brazil, enabled=True)
-        CityFactory.create(name="Maringá", state=parana, enabled=True)
+        cities = {
+            "maringa": CityFactory.create(name="Maringá", state=parana, enabled=True),
+            "umuarama": CityFactory.create(name="Umuarama", state=parana, enabled=True),
+            "sarandi": CityFactory.create(name="Sarandi", state=parana, enabled=True),
+            "iguatemi": CityFactory.create(name="Iguatemi", state=parana, enabled=True),
+        }
 
         # Cargos e permissoes
         groups = []
@@ -29,66 +66,41 @@ class Command(BaseCommand):
             groups.append(GroupFactory.create(name=g))
 
         # Estados civis
+        mar_stats = {}
         for m in MARITAL_STATUSES:
-            MaritalStatusFactory.create(name=m, enabled=True)
+            mar_stats[m] = MaritalStatusFactory.create(name=m, enabled=True)
 
         # Programas sociais
+        soc_progs = {}
         for s in SOCIAL_PROGRAMS:
-            SocialProgramFactory.create(name=s, enabled=True)
+            soc_progs[s] = SocialProgramFactory.create(name=s, enabled=True)
 
         # Status
+        statuses = {}
         for s in STATUSES:
-            StatusFactory.create(name=s, enabled=True)
+            statuses[s] = StatusFactory.create(name=s, enabled=True)
         
+        # Tamanhos de sapato
+        shoe_sizes = {}
         for s in SHOE_SIZES:
-            SizeFactory.create(name=s, type=SHOE_TYPE)
+            shoe_sizes[s] = SizeFactory.create(name=s, type=SHOE_TYPE)
         
+        # Tamanhos de roupa
+        cloth_sizes = {}
         for s in CLOTH_SIZES:
-            SizeFactory.create(name=s, type=CLOTH_TYPE)
+            cloth_sizes[s] = SizeFactory.create(name=s, type=CLOTH_TYPE)
         
-        admin_user = UserFactory.create(username="00000000000", password="admin", first_name="Administradora")
+        admin_user = UserFactory.create(**ADMIN_DATA)
         admin_user.groups.set(groups)
-        VolunteerFactory.create(user=admin_user)
+        VolunteerFactory.create(user=admin_user, city=cities['maringa'])
+        
+        for ben in BENEFICIARIES:
+            self.create_beneficiary(ben, cities, mar_stats, soc_progs)
 
-        if not options['test']:
-            return
-
-        # Beneficiárias esperando por aprovação
-        for i in range(5):
-            identification = f"ben_pending_{i}"
-            u: User = UserFactory.create(username=identification, password=identification, first_name=identification)
-
-            BeneficiaryFactory.create(user=u, approved=False)
-
-        # Beneficiárias com filhos nascidos
-        for i in range(2):
-            identification = f"ben_child_{i}"
-            u: User = UserFactory.create(username=identification, password=identification, first_name=identification)
-            b = BeneficiaryFactory.create(user=u)
-            ChildFactory.create(beneficiary=b)
-
-        # Beneficiárias grávidas
-        for i in range(2):
-            identification = f"ben_pregnant_{i}"
-            u: User = UserFactory.create(username=identification, password=identification, first_name=identification)
-            b = BeneficiaryFactory.create(user=u)
-            
-            bdate = now() + timedelta(weeks=randint(3, 49))
-            ChildFactory.create(beneficiary=b, birth_date=bdate)
-
-        # Beneficiárias com programas sociais
-        for i in range(2):
-            identification = f"ben_social_{i}"
-            u: User = UserFactory.create(username=identification, password=identification, first_name=identification)
-            b = BeneficiaryFactory.create(user=u)
-            b.social_programs.add(*SocialProgramFactory.create_batch(2))
-
-        # Beneficiárias com trocas
-        for i in range(2):
-            identification = f"ben_swap_{i}"
-            u: User = UserFactory.create(username=identification, password=identification, first_name=identification)
-            b = BeneficiaryFactory.create(user=u)
-            SwapFactory.create(beneficiary=b, child=ChildFactory.create(beneficiary=b))
+        for ben_swap in SWAP_BENEFICIARIES:
+            swap_data = ben_swap.pop("swap_data")
+            ben, children = self.create_beneficiary(ben_swap, cities, mar_stats, soc_progs)
+            self.create_swap(swap_data, ben, children[0], shoe_sizes, cloth_sizes, statuses)
 
         # Beneficiária com prontuário com 5 registros
         identification = f"ben_registries"
